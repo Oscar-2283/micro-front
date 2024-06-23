b92472c73aaec171a825bbdd361984e405b34730
Oscar Wang <>
2024-06-23 19:56:58 +0800

    新增 Vue 相關依賴庫和配置

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,38 +1,52 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const {ModuleFederationPlugin} = require("webpack").container;
-const path = require("path");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
 
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./public/index.html",
-  filename: "./index.html"
+  template: './public/index.html',
+  filename: './index.html',
 });
 module.exports = {
   mode: 'development',
+  entry: {
+    main: './src/index.js',
+    vue: './src/vue/index.js',
+  },
   devServer: {
-    static: path.join(__dirname, "dist"),
+    static: path.join(__dirname, 'dist'),
     port: 3001,
-    historyApiFallback:{
-      index:'/public/index.html'
+    historyApiFallback: {
+      index: '/public/index.html',
     },
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader"
-      }
-    }
-    ]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+    ],
   },
   plugins: [
     htmlPlugin,
+    new VueLoaderPlugin(),
     new ModuleFederationPlugin({
-      name: "container",
-      filename: "remoteEntry.js",
+      name: 'container',
+      filename: 'remoteEntry.js',
       remotes: {
-        MicroFrontend: "MicroFrontend@http://localhost:3000/remoteEntry.js"
-      }
-    })
-  ]
+        MicroFrontend: 'MicroFrontend@http://localhost:3000/remoteEntry.js',
+        // VueMicroFrontend: "VueMicroFrontend@http://localhost:3002/remoteEntry.js"
+      },
+    }),
+  ],
 };
